22f2693d0b73da70b31f4c01b1f81e8a6b7b8d9e
Débora  Fernandes Costa <116604446+deborabckp@users.noreply.github.com>
2023-03-02 15:35:20 -0300

    Create acessocaracter.c

---
+++ arquivos/acessocaracter.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+    int c;
+    char entrada[121];
+    char saida[121];
+    FILE *e;
+    FILE *s;
+    
+    printf("Digite o nome do arquivo de entrada:\t");
+    scanf("%120s", entrada);
+    printf("Digite o nome do arquivo de saida:\t");
+    scanf("%120s", saida);
+
+    e = fopen(entrada, "rt");
+    if(e == NULL){
+        printf("Erro, arquivo nao encontrado\n");
+        exit(1);
+    }
+    
+    s = fopen(saida, "wt");
+    if(s == NULL){
+        printf("Erro, arquivo nao encontrado\n");
+        fclose(e);
+        exit(1);
+    }
+
+    while((c = fgetc(e)) != EOF){
+        fputc(toupper(c), s);
+    }
+
+    fclose(e);
+    fclose(s);
+
+    return 0;
+}
